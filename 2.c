662fe456cdf4dc6529541f57a8955af1fc7824e2
rid-hima10 <149230023+rid-hima10@users.noreply.github.com>
2023-12-05 22:24:29 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+//area of circle
+#include<stdio.h>
+int main()
+{
+    int r;
+    float pi=3.14;
+    printf("Enter radius");
+    scanf("%d",&r);
+    printf("Area is %f",pi*r*r);
+    return 0;
+}

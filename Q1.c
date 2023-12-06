dc59add9340bb286370f01b3abdec9cb253320ce
rid-hima10 <149230023+rid-hima10@users.noreply.github.com>
2023-12-06 22:26:24 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n % 3 == 0) {
+        printf("divisible by 3\n");
+    } else {
+        printf("not divisible by 3\n");
+    }
+    if (n % 7 == 0) {
+        printf("divisible by 7\n");
+    } else {
+        printf("not divisible by 7\n");
+    }
+    if (n % 5 == 0) {
+        printf("divisible by 5\n");
+    } else {
+        printf("not divisible by 5\n");
+    }
+    if (n % 8 == 0) {
+        printf("divisible by 8\n");
+    } else {
+        printf("not divisible by 8\n");
+    }
+    if (n % 6 == 0) {
+        printf("divisible by 6\n");
+    } else {
+        printf("not divisible by 6\n");
+    }
+    return 0;
+}
